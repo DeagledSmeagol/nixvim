1c330981369d4c395a0633b3d1723e4e0315db8a
Alex Coukoulis <alex@coukoulis.com>
2026-07-22 20:59:38 -0500

    remove cmp-emoji

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -1,8 +1,5 @@
 {
   plugins = {
-    cmp-emoji = {
-      enable = true;
-    };
     cmp = {
       enable = true;
       settings = {
@@ -26,9 +23,8 @@
           ];
         };
         sources = [
-          { name = "git"; }
-          { name = "nvim_lsp"; }
-          { name = "emoji"; }
+          {name = "git";}
+          {name = "nvim_lsp";}
           {
             name = "buffer"; # text within current buffer
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
