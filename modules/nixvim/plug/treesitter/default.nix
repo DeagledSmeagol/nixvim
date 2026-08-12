880689c70417528c348647036cff9e4871b9fa0c
Alex Coukoulis <alex@coukoulis.com>
2026-08-12 00:20:57 -0500

    fix treesitter
    Extra lua config was breaking init.lua. need to look into this more

--- modules/nixvim/plug/treesitter/default.nix
+++ modules/nixvim/plug/treesitter/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   filetype.extension.liq = "liquidsoap";
 
   plugins.treesitter = {
@@ -14,17 +13,9 @@
       };
     };
 
-    folding = true;
+    folding.enable = true;
     languageRegister.liq = "liquidsoap";
     nixvimInjections = true;
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
   };
-
-  extraConfigLua = ''
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.liquidsoap = {
-      filetype = "liquidsoap",
-    }
-  '';
 }
